cc75d1cf882ff2221cd0857e6637be880ddbde97
NullVoxPopuli <199018+NullVoxPopuli@users.noreply.github.com>
2023-07-26 20:24:46 -0400

    Apply changes needed to run vite in strictish/compatish mode

--- ember-cli-build.js
+++ ember-cli-build.js
@@ -1,18 +1,22 @@
 'use strict';
 
 const EmberApp = require('ember-cli/lib/broccoli/ember-app');
+const { compatBuild } = require('@embroider/compat');
 
 module.exports = function (defaults) {
   const app = new EmberApp(defaults, {
     // Add options here
   });
 
-  const { Webpack } = require('@embroider/webpack');
-  return require('@embroider/compat').compatBuild(app, Webpack, {
-    skipBabel: [
-      {
-        package: 'qunit',
-      },
-    ],
+  return compatBuild(app, undefined, {
+    staticAddonTrees: true,
+    staticAddonTestSupportTrees: true,
+    staticComponents: true,
+    staticHelpers: true,
+    staticModifiers: true,
+    staticEmberSource: true,
+    amdCompatibility: {
+      es: [],
+    },
   });
 };
